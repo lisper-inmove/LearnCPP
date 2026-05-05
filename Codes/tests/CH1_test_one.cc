ae6c7514bc1c301586f32ce02341602e5bb2bb61
inmove <lisper.inmove@gmail.com>
2026-05-06 00:02:56 +0800

    First loop

--- Codes/tests/CH1_test_one.cc
+++ Codes/tests/CH1_test_one.cc
@@ -1,11 +1,16 @@
 #include "pch.h"
 #include "tester.h"
 #include "gtest/gtest.h"
+#include <iostream>
 
-namespace cvtest::tester
-{
-    TEST_F(Tester, TestOne)
-    {
-        EXPECT_EQ(1, 1);
-    }
+namespace cvtest::tester {
+TEST_F(Tester, _1_EmptyTest) { EXPECT_EQ(1, 1); }
+
+TEST_F(Tester, TestLoop) {
+  int sum = 0;
+  for (int i = 0; i < 10; i++) {
+    sum += i;
+  }
+  std::cout << "Sum is " << sum << "\n";
 }
+} // namespace cvtest::tester
